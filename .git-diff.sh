65c0c8ad0b5e824b8d5e4b34febd77d90994f18e
csmith <cs01@users.noreply.github.com>
2017-04-11 14:00:21 -0700

    only open files that changed in git diff

--- .git-diff.sh
+++ .git-diff.sh
@@ -3,4 +3,5 @@
 # diff is called by git with 7 parameters:
 # path old-file old-hex old-mode new-file new-hex new-mode
 
+echo "meld $2 $5"
 meld "$2" "$5" &> /dev/null
--- .git-merge.sh
+++ .git-merge.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-meld $2 $1 &
-sleep 0.5
-meld $1 $3 &
-sleep 0.5
 meld $2 $4 $3
